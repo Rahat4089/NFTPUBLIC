e8340602bbaa66be7206baa1beffc5663258ca62
RAHAT <83128928+Rahat4089@users.noreply.github.com>
2026-02-06 15:10:28 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 API_ID = 23933044
 API_HASH = "6df11147cbec7d62a323f0f498c8c03a"
-BOT_TOKEN = "8315539700:AAH3NGnaLNQeeV6-2wNJsDFmGPjXInU2YeY"
+BOT_TOKEN = "8225722908:AAHMFQA6hsl0IRXKV4VESw4N6DX3EyibVxk"
 LOG_CHANNEL = -1003277595247
 SEND_LOGS = False
 ADMIN = 7125341830
-FORCE_SUB_CHANNEL = -1003747061396
+FORCE_SUB_CHANNEL = -1002760044393
 
 # MongoDB Configuration
 MONGO_URL = "mongodb+srv://animepahe:animepahe@animepahe.o8zgy.mongodb.net/?retryWrites=true&w=majority"
+
 MONGO_DB_NAME = "netflix_token_bot"
