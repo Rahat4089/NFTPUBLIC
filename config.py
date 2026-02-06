1cf10e10b4cd09769517380a377aedb9b3bd69d5
RAHAT <83128928+Rahat4089@users.noreply.github.com>
2026-02-06 14:56:57 +0600

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+API_ID = 23933044
+API_HASH = "6df11147cbec7d62a323f0f498c8c03a"
+BOT_TOKEN = "8315539700:AAH3NGnaLNQeeV6-2wNJsDFmGPjXInU2YeY"
+LOG_CHANNEL = -1003277595247
+SEND_LOGS = False
+ADMIN = 7125341830
+FORCE_SUB_CHANNEL = -1003747061396
+
+# MongoDB Configuration
+MONGO_URL = "mongodb+srv://animepahe:animepahe@animepahe.o8zgy.mongodb.net/?retryWrites=true&w=majority"
+MONGO_DB_NAME = "netflix_token_bot"
