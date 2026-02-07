4dbe3959aeec64fea9a8bbd5e6045602f82440d0
RAHAT <83128928+Rahat4089@users.noreply.github.com>
2026-02-07 14:45:41 +0600

    Refactor admin configuration in config.py
    Updated ADMIN to ADMINS for multiple admin support.

--- config.py
+++ config.py
@@ -3,10 +3,9 @@ API_HASH = "6df11147cbec7d62a323f0f498c8c03a"
 BOT_TOKEN = "8225722908:AAHMFQA6hsl0IRXKV4VESw4N6DX3EyibVxk"
 LOG_CHANNEL = -1003277595247
 SEND_LOGS = False
-ADMIN = 7125341830
+ADMINS = [7125341830]  # Add multiple admins here
 FORCE_SUB_CHANNEL = -1002760044393
 
 # MongoDB Configuration
 MONGO_URL = "mongodb+srv://animepahe:animepahe@animepahe.o8zgy.mongodb.net/?retryWrites=true&w=majority"
-
 MONGO_DB_NAME = "netflix_token_bot"
